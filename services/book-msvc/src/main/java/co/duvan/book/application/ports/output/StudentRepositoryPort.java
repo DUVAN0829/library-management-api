8e3d384d4403b8936e4014a29eea992c8ad26cfa
DUVAN0829 <duvthan067@gmail.com>
2026-02-03 14:53:57 -0500

    feat(book-msvc): Añade repositorio 'StudentRepositoryPort' para manejar persistencia

---
+++ services/book-msvc/src/main/java/co/duvan/book/application/ports/output/StudentRepositoryPort.java
@@ -0,0 +1,18 @@
+package co.duvan.book.application.ports.output;
+
+import co.duvan.book.domain.model.Book;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface StudentRepositoryPort {
+
+    Optional<Book> findById(Long id);
+
+    Book save(Book book);
+
+    List<Book> findAll();
+
+    void deleteById(Long id);
+
+}
