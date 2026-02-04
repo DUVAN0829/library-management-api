531c864b3055a62c47f12e581d2ead54a28ab73d
DUVAN0829 <duvthan067@gmail.com>
2026-02-04 09:43:31 -0500

    refactor(book-msvc): Refactoriza test unitario 'CreateBookUseCaseImpl'

--- services/book-msvc/src/test/java/co/duvan/book/application/usecase/CreateBookUseCaseImplTest.java
+++ services/book-msvc/src/test/java/co/duvan/book/application/usecase/CreateBookUseCaseImplTest.java
@@ -26,41 +26,19 @@ class CreateBookUseCaseImplTest {
     @Test
     void should_create_book_successfully() {
 
-        //* Arrange
-        Book book = new Book();
-        book.setTitle("Joyland");
-        book.setDescription("A young college student takes a summer job at an amusement park, which hides a dark secret.");
-        book.setAuthors(List.of("Stephen King"));
-        book.setCategory(Category.THRILLER);
-        book.setPublisher("Scribner");
+       Book book = new Book();
 
-        Book bookDB = new Book();
-        bookDB.setBookId(1L);
-        bookDB.setTitle(book.getTitle());
-        bookDB.setIsbn("ISBN-" + bookDB.getBookId());
-        bookDB.setDescription(book.getDescription());
-        bookDB.setAuthors(book.getAuthors());
-        bookDB.setCategory(book.getCategory());
-        bookDB.setPublisher(book.getPublisher());
+       Book saveBook = new Book();
+       saveBook.setBookId(1L);
 
-        when(repositoryPort.save(book)).thenReturn(bookDB);
+       when(repositoryPort.save(book)).thenReturn(saveBook);
 
-        //* Act
-        Book result = createBookUseCase.save(book);
+       Book result = createBookUseCase.save(book);
 
-        //* Assert
-        assertAll(
-                () -> assertNotNull(result),
-                () -> assertEquals(1L, result.getBookId()),
-                () -> assertEquals("Joyland", result.getTitle()),
-                () -> assertEquals("A young college student takes a summer job at an amusement park, which hides a dark secret.", result.getDescription()),
-                () -> assertEquals("ISBN-" + result.getBookId(), result.getIsbn()),
-                () -> assertNotNull(result.getAuthors()),
-                () -> assertEquals(Category.THRILLER, result.getCategory()),
-                () -> assertEquals("Scribner", result.getPublisher())
-        );
+       assertNotNull(result);
+       assertNotNull(result.getBookId());
 
-        verify(repositoryPort, times(1)).save(book);
+       verify(repositoryPort).save(book);
 
     }
 
---
+++ services/book-msvc/src/test/java/co/duvan/book/application/usecase/GetBookUseCaseImplTest.java
@@ -0,0 +1,19 @@
+package co.duvan.book.application.usecase;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class GetBookUseCaseImplTest {
+
+    @Test
+    void should_find_book_successfully() {
+
+
+
+    }
+
+}
