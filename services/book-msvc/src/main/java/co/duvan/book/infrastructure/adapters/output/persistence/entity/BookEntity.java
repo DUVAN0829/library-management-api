e97af4008b58c29c6cbdba59b8da6f91a01c85a6
DUVAN0829 <duvthan067@gmail.com>
2026-02-04 16:06:45 -0500

    feat(book-msvc): Añade entidad 'BookEntity' en capa infrastructure

---
+++ services/book-msvc/src/main/java/co/duvan/book/infrastructure/adapters/output/persistence/entity/BookEntity.java
@@ -0,0 +1,43 @@
+package co.duvan.book.infrastructure.adapters.output.persistence.entity;
+
+import co.duvan.book.domain.enums.Category;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "books")
+public class BookEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(nullable = false, unique = true)
+    private String isbn;
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    @ElementCollection
+    @CollectionTable(
+            name = "book_authors",
+            joinColumns = @JoinColumn(name = "book_id")
+    )
+    @Column(name = "author")
+    private List<String> authors;
+
+    private String publisher;
+
+}
