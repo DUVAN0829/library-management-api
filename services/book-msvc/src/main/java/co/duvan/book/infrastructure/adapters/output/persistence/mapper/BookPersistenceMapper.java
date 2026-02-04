670521c27ba5c223e02938d91152257c443181c9
DUVAN0829 <duvthan067@gmail.com>
2026-02-04 17:05:14 -0500

    feat(book-msvc): Añade mapper 'BookPersistenceMapper' en la capa 'infrastructure'

---
+++ services/book-msvc/src/main/java/co/duvan/book/infrastructure/adapters/output/persistence/mapper/BookPersistenceMapper.java
@@ -0,0 +1,18 @@
+package co.duvan.book.infrastructure.adapters.output.persistence.mapper;
+
+import co.duvan.book.domain.model.Book;
+import co.duvan.book.infrastructure.adapters.output.persistence.entity.BookEntity;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface BookPersistenceMapper {
+
+    BookEntity toBookEntity(Book book);
+
+    Book toBook(BookEntity bookEntity);
+
+    List<Book> toListBook(List<BookEntity> listBookEntity);
+
+}
