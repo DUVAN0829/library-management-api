02c521cc7d31692271f8f2dcca4ba25138d0ff53
DUVAN0829 <duvthan067@gmail.com>
2026-02-04 22:21:50 -0500

    feat(book-msvc): Añade adaptador 'BookPersistenceAdapter'

---
+++ services/book-msvc/src/main/java/co/duvan/book/infrastructure/adapters/output/persistence/BookPersistenceAdapter.java
@@ -0,0 +1,41 @@
+package co.duvan.book.infrastructure.adapters.output.persistence;
+
+import co.duvan.book.application.ports.output.BookRepositoryPort;
+import co.duvan.book.domain.model.Book;
+import co.duvan.book.infrastructure.adapters.output.persistence.entity.BookEntity;
+import co.duvan.book.infrastructure.adapters.output.persistence.mapper.BookPersistenceMapper;
+import co.duvan.book.infrastructure.adapters.output.persistence.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class BookPersistenceAdapter implements BookRepositoryPort {
+
+    private final BookPersistenceMapper mapper;
+    private final BookRepository repository;
+
+    @Override
+    public Optional<Book> findById(Long id) {
+        return repository.findById(id).map(mapper::toBook);
+    }
+
+    @Override
+    public Book save(Book book) {
+        return mapper.toBook(repository.save(mapper.toBookEntity(book)));
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return mapper.toListBook((List<BookEntity>) repository.findAll());
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        repository.deleteById(id);
+    }
+
+}
