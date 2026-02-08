92baf9fb8a817e9f55c88dd03cc870e5e2b5d3c4
DUVAN0829 <duvthan067@gmail.com>
2026-02-08 12:02:22 -0500

    feat(book-msvc): Añade BookRestMapper para mapear requests y responses del controlador

---
+++ services/book-msvc/src/main/java/co/duvan/book/infrastructure/adapters/input/rest/mapper/BookRestMapper.java
@@ -0,0 +1,20 @@
+package co.duvan.book.infrastructure.adapters.input.rest.mapper;
+
+import co.duvan.book.domain.model.Book;
+import co.duvan.book.infrastructure.adapters.input.rest.model.request.BookRequest;
+import co.duvan.book.infrastructure.adapters.input.rest.model.response.BookResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface BookRestMapper {
+
+    Book toBook(BookRequest bookRequest);
+
+    BookResponse toBookResponse(BookRequest bookRequest);
+
+    List<BookResponse> toBookResponseList(List<Book> books);
+
+}
