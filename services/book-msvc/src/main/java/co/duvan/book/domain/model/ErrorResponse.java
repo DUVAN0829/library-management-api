be0892ba178c668462d65029a26ad820662bed61
DUVAN0829 <duvthan067@gmail.com>
2026-02-09 17:00:27 -0500

    feat(book-msvc): Añade ErrorResponse para mapear los errores del servidor

---
+++ services/book-msvc/src/main/java/co/duvan/book/domain/model/ErrorResponse.java
@@ -0,0 +1,20 @@
+package co.duvan.book.domain.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+public class ErrorResponse {
+
+    private String code;
+    private String message;
+    private List<String> details;
+    private LocalDateTime timestamp;
+
+}
