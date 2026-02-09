30f1370c3ed8ec7bd2a8b12c859be35e0bf52337
DUVAN0829 <duvthan067@gmail.com>
2026-02-09 18:54:10 -0500

    test(book-msvc): Añade test para excepcion 400 BAD_REQUEST

--- services/book-msvc/src/test/java/co/duvan/book/infrastructure/adapters/input/rest/GlobalControllerAdviceTest.java
+++ services/book-msvc/src/test/java/co/duvan/book/infrastructure/adapters/input/rest/GlobalControllerAdviceTest.java
@@ -6,12 +6,15 @@ import co.duvan.book.application.ports.input.GetBookUseCase;
 import co.duvan.book.application.ports.input.UpdateBookUseCase;
 import co.duvan.book.domain.exceptions.BookNotFoundException;
 import co.duvan.book.infrastructure.adapters.input.rest.mapper.BookRestMapper;
+import co.duvan.book.infrastructure.adapters.input.rest.model.request.BookRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import tools.jackson.databind.ObjectMapper;
 
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -21,6 +24,8 @@ import static org.junit.jupiter.api.Assertions.*;
 @WebMvcTest(BookRestAdapter.class)
 @Import(GlobalControllerAdvice.class)
 class GlobalControllerAdviceTest {
+
+    private static final String BASE_URL = "/books/api/v1";
 
     @Autowired
     private MockMvc mockMvc;
@@ -39,6 +44,8 @@ class GlobalControllerAdviceTest {
 
     @MockitoBean
     private BookRestMapper bookRestMapper;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
     void should_return_404_when_book_not_found() throws Exception {
@@ -56,5 +63,39 @@ class GlobalControllerAdviceTest {
 
     }
 
+    @Test
+    void should_return_400_when_book_request_is_invalid() throws Exception {
+
+        //* Given
+        BookRequest invalidRequest = new BookRequest();
+        invalidRequest.setTitle("");
+
+        //* When
+        mockMvc.perform(post(BASE_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(invalidRequest)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value("ERR_BOOK_02"))
+                .andExpect(jsonPath("$.message").value("Invalid book parameters"))
+                .andExpect(jsonPath("$.details").isArray())
+                .andExpect(jsonPath("$.details[?(@ == 'Field title cannot be blank')]").exists());
+
+    }
+
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
