0765d32f9a6de3823c9f9d24674d0aa4226f3dc9
DUVAN0829 <duvthan067@gmail.com>
2026-02-11 12:00:55 -0500

    build(user-msvc): Añade dependencias para mapstruct

--- services/book-msvc/src/main/java/co/duvan/book/infrastructure/adapters/output/persistence/BookPersistenceAdapter.java
+++ services/book-msvc/src/main/java/co/duvan/book/infrastructure/adapters/output/persistence/BookPersistenceAdapter.java
@@ -2,7 +2,6 @@ package co.duvan.book.infrastructure.adapters.output.persistence;
 
 import co.duvan.book.application.ports.output.BookRepositoryPort;
 import co.duvan.book.domain.model.Book;
-import co.duvan.book.infrastructure.adapters.output.persistence.entity.BookEntity;
 import co.duvan.book.infrastructure.adapters.output.persistence.mapper.BookPersistenceMapper;
 import co.duvan.book.infrastructure.adapters.output.persistence.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
@@ -15,22 +14,22 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class BookPersistenceAdapter implements BookRepositoryPort {
 
-    private final BookPersistenceMapper mapper;
+    private final BookPersistenceMapper bookPersistenceMapper;
     private final BookRepository repository;
 
     @Override
     public Optional<Book> findById(Long id) {
-        return repository.findByBookId(id).map(mapper::toBook);
+        return repository.findByBookId(id).map(bookPersistenceMapper::toBook);
     }
 
     @Override
     public Book save(Book book) {
-        return mapper.toBook(repository.save(mapper.toBookEntity(book)));
+        return bookPersistenceMapper.toBook(repository.save(bookPersistenceMapper.toBookEntity(book)));
     }
 
     @Override
     public List<Book> findAll() {
-        return mapper.toListBook(repository.findAll());
+        return bookPersistenceMapper.toListBook(repository.findAll());
     }
 
     @Override
