78a6631557ed486273bbe04964b69f984d8f910b
DUVAN0829 <duvthan067@gmail.com>
2026-02-11 11:43:34 -0500

    feat(user-msvc): Implementa enum para género de personas

--- services/user-msvc/src/main/java/co/duvan/user/domain/enums/Gender.java
+++ services/user-msvc/src/main/java/co/duvan/user/domain/enums/Gender.java
@@ -1,4 +1,18 @@
 package co.duvan.user.domain.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum Gender {
+
+    FEMALE("Female"),
+    MALE("Male"),
+    OTHER("Other");
+
+    private final String value;
+
+    Gender(String value) {
+        this.value = value;
+    }
+
 }
