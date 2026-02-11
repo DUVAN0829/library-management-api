ce7ebd6e84dcb7edee6416da34a11ac37fd6960d
DUVAN0829 <duvthan067@gmail.com>
2026-02-11 11:41:15 -0500

    feat(user-msvc): Implementa enum para tipo de documentos

--- services/user-msvc/src/main/java/co/duvan/user/domain/enums/DocumentType.java
+++ services/user-msvc/src/main/java/co/duvan/user/domain/enums/DocumentType.java
@@ -1,4 +1,18 @@
 package co.duvan.user.domain.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum DocumentType {
+
+    IDENTITY_DOCUMENT("Identity Document"),
+    PASSPORT("Passport"),
+    OTHER("Other");
+
+    private final String value;
+
+    DocumentType(String value) {
+        this.value = value;
+    }
+
 }
