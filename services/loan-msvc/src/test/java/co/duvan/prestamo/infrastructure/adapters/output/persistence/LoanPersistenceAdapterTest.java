c99d4324ee6119fa9ea647c1fc20cf2d97979c8f
DUVAN0829 <duvthan067@gmail.com>
2026-02-13 15:08:28 -0500

    build(loan-msvc): Añade dependencias para test container con postgreSQL

---
+++ services/loan-msvc/src/test/java/co/duvan/prestamo/infrastructure/adapters/output/persistence/LoanPersistenceAdapterTest.java
@@ -0,0 +1,10 @@
+package co.duvan.prestamo.infrastructure.adapters.output.persistence;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@Testcontainers
+@SpringBootTest
+class LoanPersistenceAdapterTest {
+
+}
