5a9f6e2bc516a0ef3d4af9915f9ae10415df78e8
DUVAN0829 <duvthan067@gmail.com>
2026-02-13 14:34:30 -0500

    feat(loan-msvc): Añade mapper para persistencia

--- services/loan-msvc/src/main/java/co/duvan/prestamo/infrastructure/adapters/output/persistence/mapper/LoanPersitenceMapper.java
+++ services/loan-msvc/src/main/java/co/duvan/prestamo/infrastructure/adapters/output/persistence/mapper/LoanPersitenceMapper.java
@@ -1,4 +1,18 @@
 package co.duvan.prestamo.infrastructure.adapters.output.persistence.mapper;
 
+import co.duvan.prestamo.domain.model.Loan;
+import co.duvan.prestamo.infrastructure.adapters.output.persistence.entity.LoanEntity;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
 public interface LoanPersitenceMapper {
+
+    LoanEntity toLoanEntity(Loan loan);
+
+    Loan toLoan(LoanEntity loanEntity);
+
+    List<Loan> toListLoan(List<LoanEntity> loanEntityList);
+
 }
