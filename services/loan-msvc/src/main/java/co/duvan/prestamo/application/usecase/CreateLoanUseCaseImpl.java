494feddfa4be914376b4c5349e3f1ccae6d32e76
DUVAN0829 <duvthan067@gmail.com>
2026-02-13 12:19:57 -0500

    feat(loan-msvc): Añade implementación de caso de uso para crear un prestamo

---
+++ services/loan-msvc/src/main/java/co/duvan/prestamo/application/usecase/CreateLoanUseCaseImpl.java
@@ -0,0 +1,18 @@
+package co.duvan.prestamo.application.usecase;
+
+import co.duvan.prestamo.application.ports.input.CreateLoanUseCase;
+import co.duvan.prestamo.application.ports.output.LoanRepositoryPort;
+import co.duvan.prestamo.domain.model.Loan;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class CreateLoanUseCaseImpl implements CreateLoanUseCase {
+
+    private final LoanRepositoryPort repositoryPort;
+
+    @Override
+    public Loan save(Loan loan) {
+        return repositoryPort.save(loan);
+    }
+
+}
