13da014fe785b2335fa15154f0d7074708a4f147
DUVAN0829 <duvthan067@gmail.com>
2026-02-13 11:41:04 -0500

    feat(loan-msvc): Añade configuración inicial para loan-msvc

---
+++ services/loan-msvc/src/test/java/co/duvan/prestamo/PrestamoMsvcApplicationTests.java
@@ -0,0 +1,13 @@
+package co.duvan.prestamo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrestamoMsvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
