419e1a95132c486f8c4b42704cf658361d1d25e9
DUVAN0829 <duvthan067@gmail.com>
2026-02-13 09:25:38 -0500

    feat(user-msvc): Añade ErrorResponse para mapear excepciones del servidor

---
+++ services/user-msvc/src/main/java/co/duvan/user/infrastructure/adapters/input/rest/GlobalControllerAdvice.java
@@ -0,0 +1,10 @@
+package co.duvan.user.infrastructure.adapters.input.rest;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalControllerAdvice {
+
+
+
+}
