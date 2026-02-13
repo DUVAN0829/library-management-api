5822f1b448b34b2b4078ee3d885867126241452c
DUVAN0829 <duvthan067@gmail.com>
2026-02-13 14:12:24 -0500

    feat(loan-msvc): Añade entidad prestamo para mapear base de datos

---
+++ services/loan-msvc/src/main/java/co/duvan/prestamo/infrastructure/adapters/output/persistence/entity/LoanEntity.java
@@ -0,0 +1,31 @@
+package co.duvan.prestamo.infrastructure.adapters.output.persistence.entity;
+
+import co.duvan.prestamo.domain.enums.Status;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "loans")
+public class LoanEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long loanId;
+
+    private Long userId;
+    private Long copyId;
+    private LocalDate loanDate;
+    private LocalDate dueDate;
+    private LocalDate returnDate;
+    private Status loanStatus;
+
+}
