6a85928eefc00a8e927789713c5217a64263921d
DUVAN0829 <duvthan067@gmail.com>
2026-02-13 12:40:03 -0500

    feat(loan-msvc): Añade implementación de caso de uso obtener todos los prestamos y obtener prestamo por id

---
+++ services/loan-msvc/src/main/java/co/duvan/prestamo/application/usecase/GetLoanUseCaseImpl.java
@@ -0,0 +1,41 @@
+package co.duvan.prestamo.application.usecase;
+
+import co.duvan.prestamo.application.ports.input.GetLoanUseCase;
+import co.duvan.prestamo.application.ports.output.LoanRepositoryPort;
+import co.duvan.prestamo.domain.exceptions.LoanNotFoundException;
+import co.duvan.prestamo.domain.model.Loan;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class GetLoanUseCaseImpl implements GetLoanUseCase {
+
+    private final LoanRepositoryPort repositoryPort;
+
+    @Override
+    public Loan findById(Long id) {
+
+        return repositoryPort.findById(id)
+                .orElseThrow(() -> new LoanNotFoundException("Loan not found with id: " + id));
+    }
+
+    @Override
+    public List<Loan> findAll() {
+        return repositoryPort.findAll();
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
