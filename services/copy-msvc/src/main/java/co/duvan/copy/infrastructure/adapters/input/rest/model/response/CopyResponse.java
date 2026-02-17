1f5056810e30a57603603ad9362016f04d0a3cb4
DUVAN0829 <duvthan067@gmail.com>
2026-02-17 17:57:35 -0500

    feat(copy-msvc): Añade documentación para CopyResponse con swagger

--- services/copy-msvc/src/main/java/co/duvan/copy/infrastructure/adapters/input/rest/model/response/CopyResponse.java
+++ services/copy-msvc/src/main/java/co/duvan/copy/infrastructure/adapters/input/rest/model/response/CopyResponse.java
@@ -1,6 +1,8 @@
 package co.duvan.copy.infrastructure.adapters.input.rest.model.response;
 
 import co.duvan.copy.domain.enums.Status;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,11 +12,38 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(
+        name = "Copy Response",
+        description = "Copy data returned by the API"
+)
 public class CopyResponse {
 
+    @Schema(
+            description = "copyId of the book",
+            example = "2",
+            requiredMode = Schema.RequiredMode.REQUIRED
+    )
     private Long copyId;
+
+    @Schema(
+            description = "bookId of the book",
+            example = "3",
+            requiredMode = Schema.RequiredMode.REQUIRED
+    )
     private Long bookId;
+
+    @Schema(
+            description = "code of the copy",
+            example = "CP-12",
+            requiredMode = Schema.RequiredMode.REQUIRED
+    )
     private String code;
+
+    @Schema(
+            description = "status of the copy",
+            example = "AVAILABLE",
+            requiredMode = Schema.RequiredMode.REQUIRED
+    )
     private Status status;
 
 }
