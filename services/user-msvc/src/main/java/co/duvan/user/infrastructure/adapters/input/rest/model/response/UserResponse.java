253b31504cc6f631f1d67e10145d5b0504d672d8
DUVAN0829 <duvthan067@gmail.com>
2026-02-18 11:17:06 -0500

    feat(user-msvc): Añade documentación swagger para UserResponse

--- services/user-msvc/src/main/java/co/duvan/user/infrastructure/adapters/input/rest/model/response/UserResponse.java
+++ services/user-msvc/src/main/java/co/duvan/user/infrastructure/adapters/input/rest/model/response/UserResponse.java
@@ -2,6 +2,7 @@ package co.duvan.user.infrastructure.adapters.input.rest.model.response;
 
 import co.duvan.user.domain.enums.DocumentType;
 import co.duvan.user.domain.enums.Gender;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,17 +14,80 @@ import java.time.LocalDate;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(
+        name = "User Response",
+        description = "User data returned by the API"
+)
 public class UserResponse {
 
+    @Schema(
+            description = "userId of the user",
+            example = "5",
+            requiredMode = Schema.RequiredMode.REQUIRED
+    )
     private Long userId;
+
+    @Schema(
+            description = "firstname of the user",
+            example = "Duván",
+            requiredMode = Schema.RequiredMode.REQUIRED
+    )
     private String firstname;
+
+    @Schema(
+            description = "lastname of the user",
+            example = "González",
+            requiredMode = Schema.RequiredMode.REQUIRED
+    )
     private String lastname;
+
+    @Schema(
+            description = "documentType of the user",
+            example = "IDENTITY_DOCUMENT",
+            requiredMode = Schema.RequiredMode.REQUIRED
+    )
     private DocumentType documentType;
+
+    @Schema(
+            description = "documentNumber of the user",
+            example = "32.984.032",
+            requiredMode = Schema.RequiredMode.REQUIRED
+    )
     private String documentNumber;
+
+    @Schema(
+            description = "birthdate of the user",
+            example = "2004-02-11",
+            requiredMode = Schema.RequiredMode.REQUIRED
+    )
     private LocalDate birthdate;
+
+    @Schema(
+            description = "documentType of the user",
+            example = "MALE",
+            requiredMode = Schema.RequiredMode.REQUIRED
+    )
     private Gender gender;
+
+    @Schema(
+            description = "documentType of the user",
+            example = "duvan@gmail.com",
+            requiredMode = Schema.RequiredMode.REQUIRED
+    )
     private String email;
+
+    @Schema(
+            description = "documentType of the user",
+            example = "318-936-812",
+            requiredMode = Schema.RequiredMode.REQUIRED
+    )
     private String phoneNumber;
+
+    @Schema(
+            description = "documentType of the user",
+            example = "CO",
+            requiredMode = Schema.RequiredMode.REQUIRED
+    )
     private String nationality;
 
 }
