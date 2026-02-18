a0def8a19e961ba9eb415e3c014465f75e241c23
DUVAN0829 <duvthan067@gmail.com>
2026-02-18 12:15:57 -0500

    feat(user-msvc): Añade anotaciones swagger en UserRestAdapter para documentar errores de servidor

--- services/user-msvc/src/main/java/co/duvan/user/infrastructure/adapters/input/rest/UserRestAdapter.java
+++ services/user-msvc/src/main/java/co/duvan/user/infrastructure/adapters/input/rest/UserRestAdapter.java
@@ -4,10 +4,15 @@ import co.duvan.user.application.ports.input.CreateUserUseCase;
 import co.duvan.user.application.ports.input.DeleteUserUseCase;
 import co.duvan.user.application.ports.input.GetUserUseCase;
 import co.duvan.user.application.ports.input.UpdateUserUseCase;
-import co.duvan.user.domain.model.User;
+import co.duvan.user.infrastructure.adapters.input.rest.documentation.DefaultApiErrors;
+import co.duvan.user.infrastructure.adapters.input.rest.documentation.ValidationApiError;
 import co.duvan.user.infrastructure.adapters.input.rest.mapper.UserRestMapper;
 import co.duvan.user.infrastructure.adapters.input.rest.model.request.UserRequest;
 import co.duvan.user.infrastructure.adapters.input.rest.model.response.UserResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -19,6 +24,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/users")
+@Tag(name = "Users API", description = "Operations basic related users (CRUD)")
 public class UserRestAdapter {
 
     private final UserRestMapper userRestMapper;
@@ -27,13 +33,22 @@ public class UserRestAdapter {
     private final UpdateUserUseCase updateUserUseCase;
     private final DeleteUserUseCase deleteUserUseCase;
 
+    @Operation(summary = "Get all users")
+    @ApiResponse(responseCode = "200", description = "List all users")
+    @DefaultApiErrors
     @GetMapping("/api/v1")
-    public ResponseEntity<List<UserResponse>> getAllUsers() {
+    public ResponseEntity<List<UserResponse>> findAll() {
 
         return ResponseEntity.ok(userRestMapper.toUserResponseList(getUserUseCase.findAll()));
 
     }
 
+    @Operation(summary = "Get user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User found"),
+            @ApiResponse(responseCode = "404", description = "User not found")
+    })
+    @DefaultApiErrors
     @GetMapping("/api/v1/{id}")
     public ResponseEntity<UserResponse> findById(@PathVariable Long id) {
 
@@ -41,23 +56,34 @@ public class UserRestAdapter {
 
     }
 
+    @Operation(summary = "Create user")
+    @ApiResponse(responseCode = "201", description = "User created")
+    @DefaultApiErrors
+    @ValidationApiError
     @PostMapping("/api/v1")
-    public ResponseEntity<UserResponse> createUser(@Valid @RequestBody UserRequest userRequest) {
+    public ResponseEntity<UserResponse> save(@Valid @RequestBody UserRequest userRequest) {
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(userRestMapper.toUserResponse(createUserUseCase.save(userRestMapper.toUser(userRequest))));
 
     }
 
+    @Operation(summary = "Update user")
+    @ApiResponse(responseCode = "200", description = "User updated")
+    @DefaultApiErrors
+    @ValidationApiError
     @PutMapping("/api/v1/{id}")
-    public ResponseEntity<UserResponse> updateUser(@PathVariable Long id, @Valid @RequestBody UserRequest userRequest) {
+    public ResponseEntity<UserResponse> update(@PathVariable Long id, @Valid @RequestBody UserRequest userRequest) {
 
         return ResponseEntity.ok(userRestMapper.toUserResponse(updateUserUseCase.update(id, userRestMapper.toUser(userRequest))));
 
     }
 
+    @Operation(summary = "Delete user")
+    @ApiResponse(responseCode = "204", description = "User deleted")
+    @DefaultApiErrors
     @DeleteMapping("/api/v1/{id}")
-    public ResponseEntity<Void> deleteUserById(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
 
         deleteUserUseCase.deleteById(id);
 
