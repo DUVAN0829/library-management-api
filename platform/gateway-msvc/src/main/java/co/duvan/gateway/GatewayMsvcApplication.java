d879d70e9c1e042c1f1cb37193608885e7eff965
DUVAN0829 <duvthan067@gmail.com>
2026-02-19 16:25:56 -0500

    feat(gateway-msvc): Añade configuración básica

---
+++ platform/gateway-msvc/src/main/java/co/duvan/gateway/GatewayMsvcApplication.java
@@ -0,0 +1,13 @@
+package co.duvan.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayMsvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayMsvcApplication.class, args);
+	}
+
+}
