4e65f9d20cc17a3b3dd5c914181a457e591bc664
DUVAN0829 <duvthan067@gmail.com>
2026-02-21 16:47:27 -0500

    feat(user-msvc): Añade adaptador para keycloak

---
+++ services/user-msvc/src/main/java/co/duvan/user/infrastructure/adapters/output/identity/KeycloakIdentityAdapter.java
@@ -0,0 +1,20 @@
+package co.duvan.user.infrastructure.adapters.output.identity;
+
+import co.duvan.user.application.ports.output.IdentityProviderPort;
+import co.duvan.user.domain.model.User;
+import co.duvan.user.infrastructure.adapters.output.identity.service.KeycloakService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class KeycloakIdentityAdapter implements IdentityProviderPort {
+
+    private final KeycloakService service;
+
+    @Override
+    public String createUser(User user, String password) {
+        return service.createUser(user, password);
+    }
+
+}
