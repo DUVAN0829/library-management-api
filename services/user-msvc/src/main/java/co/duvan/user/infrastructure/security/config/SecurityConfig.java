22782e538c7fad5ab2ed7b6bdfe977554d6a80ce
DUVAN0829 <duvthan067@gmail.com>
2026-02-22 14:46:43 -0500

    feat(user-msvc): Añade convertidor jwt en filtro de seguridad para obtener rol

--- services/user-msvc/src/main/java/co/duvan/user/infrastructure/security/config/SecurityConfig.java
+++ services/user-msvc/src/main/java/co/duvan/user/infrastructure/security/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package co.duvan.user.infrastructure.security.config;
 
+import co.duvan.user.infrastructure.security.jwt.JwtConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,14 +14,17 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthenticationConverter jwtAuthenticationConverter) {
 
-       return http
+        http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated())
-                .build();
+                .oauth2ResourceServer(oauth -> oauth
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)));
+
+        return http.build();
 
     }
 
