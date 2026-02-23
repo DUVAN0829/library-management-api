fdafa5d06f57cf954eb3f0ac1baf5d1bad9ffd8d
DUVAN0829 <duvthan067@gmail.com>
2026-02-23 10:59:41 -0500

    feat(user-msvc): Añade configuración de seguridad en swagger

--- services/user-msvc/src/main/java/co/duvan/user/infrastructure/security/config/SecurityConfig.java
+++ services/user-msvc/src/main/java/co/duvan/user/infrastructure/security/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package co.duvan.user.infrastructure.security.config;
 import co.duvan.user.infrastructure.security.jwt.JwtConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -20,6 +21,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/users/api/**").hasRole("MEMBER")
                         .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth -> oauth
                         .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)));
