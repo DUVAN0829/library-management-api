98aa2a963c9908ff77797a10464626451fc8e2f9
DUVAN0829 <duvthan067@gmail.com>
2026-02-26 08:52:50 -0500

    feat(loan-msvc): Añade implementación para CopyClientPort

---
+++ services/loan-msvc/src/main/java/co/duvan/loan/infrastructure/adapters/output/CopyClientAdapter.java
@@ -0,0 +1,18 @@
+package co.duvan.loan.infrastructure.adapters.output;
+
+import co.duvan.loan.application.ports.output.CopyClientPort;
+import co.duvan.loan.application.ports.output.dto.CopyClientResponse;
+import co.duvan.loan.infrastructure.adapters.output.client.CopyFeignClient;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class CopyClientAdapter implements CopyClientPort {
+
+    private final CopyFeignClient copyFeignClient;
+
+    @Override
+    public CopyClientResponse findCopyById(Long copyId, String token) {
+        return copyFeignClient.findById(copyId);
+    }
+
+}
