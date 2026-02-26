fbb6a925abca7d504310080e617adf5fb7555453
DUVAN0829 <duvthan067@gmail.com>
2026-02-26 06:17:54 -0500

    feat(loan-msvc): Añade convertidor de bearer token para obtener roles

---
+++ services/loan-msvc/src/main/java/co/duvan/loan/infrastructure/security/jwt/JwtConverter.java
@@ -0,0 +1,54 @@
+package co.duvan.loan.infrastructure.security.jwt;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+
+import java.util.List;
+import java.util.Map;
+
+@Configuration
+@EnableWebSecurity
+public class JwtConverter {
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+
+        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter =
+                new JwtGrantedAuthoritiesConverter();
+
+        grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
+        grantedAuthoritiesConverter.setAuthoritiesClaimName("realm_access.roles");
+
+        JwtAuthenticationConverter authenticationConverter =
+                new JwtAuthenticationConverter();
+
+        authenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
+
+            Map<String, Object> realmAccess = jwt.getClaim("realm_access");
+            if (realmAccess == null) {
+                return List.of();
+            }
+
+            Object rolesObject = realmAccess.get("roles");
+
+            if (!(rolesObject instanceof List<?> rolesList)) {
+                return List.of();
+            }
+
+            return rolesList.stream()
+                    .filter(String.class::isInstance)
+                    .map(String.class::cast)
+                    .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                    .map(role -> (GrantedAuthority) role)
+                    .toList();
+        });
+
+        return authenticationConverter;
+    }
+
+}
