e1bff3cec069055e94566633536caf3cb7865d3f
DUVAN0829 <duvthan067@gmail.com>
2026-02-26 16:19:29 -0500

    test(loan-msvc): Corrije test GetLoanUseCaseImplTest añadiendo nuevos parámetros

--- services/loan-msvc/src/test/java/co/duvan/loan/application/usecase/GetLoanUseCaseImplTest.java
+++ services/loan-msvc/src/test/java/co/duvan/loan/application/usecase/GetLoanUseCaseImplTest.java
@@ -1,7 +1,11 @@
 package co.duvan.loan.application.usecase;
 
+import co.duvan.loan.application.ports.output.CopyClientPort;
 import co.duvan.loan.application.ports.output.LoanRepositoryPort;
+import co.duvan.loan.application.ports.output.UserClientPort;
+import co.duvan.loan.application.ports.output.dto.CopyClientResponse;
 import co.duvan.loan.application.ports.output.dto.LoanDetailResult;
+import co.duvan.loan.application.ports.output.dto.UserClientResponse;
 import co.duvan.loan.domain.exceptions.LoanNotFoundException;
 import co.duvan.loan.domain.model.Loan;
 import org.junit.jupiter.api.Test;
@@ -22,17 +26,30 @@ class GetLoanUseCaseImplTest {
     @Mock
     private LoanRepositoryPort repositoryPort;
 
+    @Mock
+    private UserClientPort userClientPort;
+
+    @Mock
+    private CopyClientPort copyClientPort;
+
     @InjectMocks
     private GetLoanUseCaseImpl getLoanUseCase;
 
     @Test
-    void should_return_loan_when_exists() {
+    void should_return_loan_detail_when_exists() {
 
         //* Arrange
         Loan loan = new Loan();
         loan.setLoanId(1L);
+        loan.setUserId(10L);
+        loan.setCopyId(20L);
+
+        UserClientResponse user = new UserClientResponse();
+        CopyClientResponse copy = new CopyClientResponse();
 
         when(repositoryPort.findById(1L)).thenReturn(Optional.of(loan));
+        when(userClientPort.findUserById(10L, null)).thenReturn(user);
+        when(copyClientPort.findCopyById(20L, null)).thenReturn(copy);
 
         //* Act
         LoanDetailResult result = getLoanUseCase.findById(1L);
@@ -40,8 +57,12 @@ class GetLoanUseCaseImplTest {
         //* Assert
         assertNotNull(result);
         assertNotNull(result.getLoan().getLoanId());
+        assertEquals(user, result.getUser());
+        assertEquals(copy, result.getCopy());
 
         verify(repositoryPort).findById(1L);
+        verify(userClientPort).findUserById(10L, null);
+        verify(copyClientPort).findCopyById(20L, null);
     }
 
     @Test
