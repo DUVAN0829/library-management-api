462d4063a9121fb1f7cdc9c0528f9828cf69eb48
DUVAN0829 <duvthan067@gmail.com>
2026-02-26 08:20:06 -0500

    feat(loan-msvc): Añade UserClientPort para obtener usuario de feign

---
+++ services/loan-msvc/src/main/java/co/duvan/loan/application/ports/output/UserClientPort.java
@@ -0,0 +1,9 @@
+package co.duvan.loan.application.ports.output;
+
+import co.duvan.loan.application.ports.output.dto.UserClientResponse;
+
+public interface UserClientPort {
+
+    UserClientResponse findUserById(Long userId, String token);
+
+}
