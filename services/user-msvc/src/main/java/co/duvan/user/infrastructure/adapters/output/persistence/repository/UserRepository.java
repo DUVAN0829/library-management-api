15ae93c23a3721bb007fa6ab1c0d06560a038dfd
DUVAN0829 <duvthan067@gmail.com>
2026-02-26 13:26:09 -0500

    feat(user-msvc): Añade método de persistencia para obtener usuario con keycloakId

--- services/user-msvc/src/main/java/co/duvan/user/infrastructure/adapters/output/persistence/repository/UserRepository.java
+++ services/user-msvc/src/main/java/co/duvan/user/infrastructure/adapters/output/persistence/repository/UserRepository.java
@@ -3,5 +3,10 @@ package co.duvan.user.infrastructure.adapters.output.persistence.repository;
 import co.duvan.user.infrastructure.adapters.output.persistence.entity.UserEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByKeycloakId(String keycloakId);
+
 }
