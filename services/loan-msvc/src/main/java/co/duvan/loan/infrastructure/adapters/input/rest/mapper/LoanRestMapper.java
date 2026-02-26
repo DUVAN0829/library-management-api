b45d7a96cbe95a81d17c3daa386081fcc262d738
DUVAN0829 <duvthan067@gmail.com>
2026-02-26 09:35:57 -0500

    feat(loan-msvc): Añade mapper para obtener usuario de feign y mostrarlo en controlador REST

--- services/loan-msvc/src/main/java/co/duvan/loan/infrastructure/adapters/input/rest/mapper/LoanRestMapper.java
+++ services/loan-msvc/src/main/java/co/duvan/loan/infrastructure/adapters/input/rest/mapper/LoanRestMapper.java
@@ -1,6 +1,8 @@
 package co.duvan.loan.infrastructure.adapters.input.rest.mapper;
 
+import co.duvan.loan.application.ports.output.dto.LoanDetailResult;
 import co.duvan.loan.domain.model.Loan;
+import co.duvan.loan.infrastructure.adapters.input.rest.model.request.LoanDetailResponse;
 import co.duvan.loan.infrastructure.adapters.input.rest.model.request.LoanRequest;
 import co.duvan.loan.infrastructure.adapters.input.rest.model.response.LoanResponse;
 import org.mapstruct.Mapper;
@@ -16,5 +18,7 @@ public interface LoanRestMapper {
     LoanResponse toLoanResponse(Loan loan);
 
     List<LoanResponse> toLoanResponseList(List<Loan> loans);
+
+    LoanDetailResponse toLoanDetailResponse(LoanDetailResult loanDetailResult);
 
 }
