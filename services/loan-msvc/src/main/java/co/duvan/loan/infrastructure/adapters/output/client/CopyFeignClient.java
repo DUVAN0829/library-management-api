5a66f983df66b78cef7a33f3d36e40e822034fa7
DUVAN0829 <duvthan067@gmail.com>
2026-02-26 08:33:52 -0500

    feat(loan-msvc): Añade client feign para ejemplar

---
+++ services/loan-msvc/src/main/java/co/duvan/loan/infrastructure/adapters/output/client/CopyFeignClient.java
@@ -0,0 +1,15 @@
+package co.duvan.loan.infrastructure.adapters.output.client;
+
+import co.duvan.loan.application.ports.output.dto.CopyClientResponse;
+import co.duvan.loan.infrastructure.feign.config.FeignClientInterceptor;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "copy-msvc", url = "http://localhost:8082", configuration = FeignClientInterceptor.class)
+public interface CopyFeignClient {
+
+    @GetMapping("/api/v1/{id}")
+    CopyClientResponse findById(@PathVariable Long id);
+
+}
