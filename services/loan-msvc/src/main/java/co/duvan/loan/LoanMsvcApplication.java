e7751d2aa79beefd49221a545d96ec61abd89e55
DUVAN0829 <duvthan067@gmail.com>
2026-02-26 07:56:57 -0500

    feat(loan-msvc): Habilita openfeign

--- services/loan-msvc/src/main/java/co/duvan/loan/LoanMsvcApplication.java
+++ services/loan-msvc/src/main/java/co/duvan/loan/LoanMsvcApplication.java
@@ -2,8 +2,10 @@ package co.duvan.loan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class LoanMsvcApplication {
 
     public static void main(String[] args) {
