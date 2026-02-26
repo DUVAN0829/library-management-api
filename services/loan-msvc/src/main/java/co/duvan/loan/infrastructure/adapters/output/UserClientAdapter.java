3d45a945a66322f26ba19d886c1aaf8b2ca8688f
DUVAN0829 <duvthan067@gmail.com>
2026-02-26 08:50:54 -0500

    feat(loan-msvc): Añade implementación para UserClientPort

---
+++ services/loan-msvc/src/main/java/co/duvan/loan/infrastructure/adapters/output/UserClientAdapter.java
@@ -0,0 +1,18 @@
+package co.duvan.loan.infrastructure.adapters.output;
+
+import co.duvan.loan.application.ports.output.UserClientPort;
+import co.duvan.loan.application.ports.output.dto.UserClientResponse;
+import co.duvan.loan.infrastructure.adapters.output.client.UserFeignClient;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class UserClientAdapter implements UserClientPort {
+
+    private final UserFeignClient userFeignClient;
+
+    @Override
+    public UserClientResponse findUserById(Long userId, String token) {
+        return userFeignClient.findById(userId);
+    }
+
+}
