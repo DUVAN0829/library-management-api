f9ae27043a698bd28d91e6eb96d4560eaa6bb40b
DUVAN0829 <duvthan067@gmail.com>
2026-02-27 15:43:09 -0500

    feat(loan-msvc): Añade implementación a caso de uso para guardar prestamo

--- services/loan-msvc/src/main/java/co/duvan/loan/application/usecase/CreateLoanUseCaseImpl.java
+++ services/loan-msvc/src/main/java/co/duvan/loan/application/usecase/CreateLoanUseCaseImpl.java
@@ -1,7 +1,12 @@
 package co.duvan.loan.application.usecase;
 
 import co.duvan.loan.application.ports.input.CreateLoanUseCase;
+import co.duvan.loan.application.ports.output.CopyClientPort;
 import co.duvan.loan.application.ports.output.LoanRepositoryPort;
+import co.duvan.loan.application.ports.output.UserClientPort;
+import co.duvan.loan.application.ports.output.dto.CopyClientResponse;
+import co.duvan.loan.application.ports.output.dto.LoanDetailResult;
+import co.duvan.loan.application.ports.output.dto.UserClientResponse;
 import co.duvan.loan.domain.model.Loan;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -11,10 +16,20 @@ import org.springframework.stereotype.Component;
 public class CreateLoanUseCaseImpl implements CreateLoanUseCase {
 
     private final LoanRepositoryPort repositoryPort;
+    private final UserClientPort userClientPort;
+    private final CopyClientPort copyClientPort;
 
     @Override
-    public Loan save(Loan loan) {
-        return repositoryPort.save(loan);
+    public LoanDetailResult save(Loan loan) {
+
+        Loan loanDb = repositoryPort.save(loan);
+
+        UserClientResponse userResponse = userClientPort.findUserById(loanDb.getUserId(), null);
+
+        CopyClientResponse copyResponse = copyClientPort.findCopyById(loanDb.getCopyId(), null);
+
+        return new LoanDetailResult(loanDb, userResponse, copyResponse);
+
     }
 
 }
