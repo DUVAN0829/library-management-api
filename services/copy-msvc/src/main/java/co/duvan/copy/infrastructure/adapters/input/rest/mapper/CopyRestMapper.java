9aaefc30c2e66c2cc97823110dc8eb8a4ec7071f
DUVAN0829 <duvthan067@gmail.com>
2026-02-26 20:27:26 -0500

    feat(copy-msvc): Añade mapper para respuesta de controllador REST

--- services/copy-msvc/src/main/java/co/duvan/copy/infrastructure/adapters/input/rest/mapper/CopyRestMapper.java
+++ services/copy-msvc/src/main/java/co/duvan/copy/infrastructure/adapters/input/rest/mapper/CopyRestMapper.java
@@ -1,9 +1,13 @@
 package co.duvan.copy.infrastructure.adapters.input.rest.mapper;
 
+import co.duvan.copy.application.ports.output.dto.CopyDetailResult;
+import co.duvan.copy.domain.enums.Status;
 import co.duvan.copy.domain.model.Copy;
 import co.duvan.copy.infrastructure.adapters.input.rest.model.request.CopyRequest;
+import co.duvan.copy.infrastructure.adapters.input.rest.model.response.CopyDetailResponse;
 import co.duvan.copy.infrastructure.adapters.input.rest.model.response.CopyResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
@@ -16,5 +20,11 @@ public interface CopyRestMapper {
     CopyResponse toCopyResponse(Copy copy);
 
     List<CopyResponse> toCopyResponseList(List<Copy> copies);
+
+    @Mapping(source = "copy.copyId", target = "copyId")
+    @Mapping(source = "copy.bookId", target = "bookId")
+    @Mapping(source = "copy.code", target = "code")
+    @Mapping(source = "copy.status", target = "status")
+    CopyDetailResponse toCopyDetailResponse(CopyDetailResult copyDetailResult);
 
 }
