926a77fe77db8d6ce0047601234d0bd7dc814bdc
DUVAN0829 <duvthan067@gmail.com>
2026-02-26 19:49:50 -0500

    feat(copy-msvc): Añade configuración de feign para acceder a microservicio de libro

---
+++ services/copy-msvc/src/main/java/co/duvan/copy/infrastructure/adapters/output/client/BookClientFeign.java
@@ -0,0 +1,15 @@
+package co.duvan.copy.infrastructure.adapters.output.client;
+
+import co.duvan.copy.application.ports.output.dto.BookClientResponse;
+import co.duvan.copy.infrastructure.feign.config.FeignClientInterceptor;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "book-msvc", url = "http://localhost:8081", configuration = FeignClientInterceptor.class)
+public interface BookClientFeign {
+
+    @GetMapping("/books/api/v1/{id}")
+    BookClientResponse getBookById(@PathVariable Long id);
+
+}
