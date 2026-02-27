0ed5aefcf7c5bae8b9831662b2c98f42dc73903e
DUVAN0829 <duvthan067@gmail.com>
2026-02-26 19:31:03 -0500

    feat(copy-msvc): Añade interceptor feign para propagar token a otros msvc

---
+++ services/copy-msvc/src/main/java/co/duvan/copy/infrastructure/feign/config/FeignClientInterceptor.java
@@ -0,0 +1,32 @@
+package co.duvan.copy.infrastructure.feign.config;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@Component
+public class FeignClientInterceptor implements RequestInterceptor {
+
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+
+    @Override
+    public void apply(RequestTemplate requestTemplate) {
+
+        ServletRequestAttributes attributes =
+                (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+
+        if (attributes != null) {
+            HttpServletRequest request = attributes.getRequest();
+            String authHeader = request.getHeader(AUTHORIZATION_HEADER);
+            if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                requestTemplate.header(AUTHORIZATION_HEADER, authHeader);
+            }
+
+        }
+
+    }
+
+}
