4d6036cb318a65616e8aded81657203916ddfcf0
DUVAN0829 <duvthan067@gmail.com>
2026-02-26 19:17:25 -0500

    feat(copy-msvc): Añade client feing para obtener libro

---
+++ services/copy-msvc/src/main/java/co/duvan/copy/application/ports/output/dto/BookClientResponse.java
@@ -0,0 +1,15 @@
+package co.duvan.copy.application.ports.output.dto;
+
+import java.util.List;
+
+public class BookClientResponse {
+
+    private Long bookId;
+    private String title;
+    private String isbn;
+    private String description;
+    private String category;
+    private List<String> authors;
+    private String publisher;
+
+}
