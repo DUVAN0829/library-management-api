c5a6d6b1b4c36ab9d0ed1697c1d1a448e2d57131
DUVAN0829 <duvthan067@gmail.com>
2026-02-28 13:27:04 -0500

    feat(book-msvc): Añade implementación de persistencia para obtener libro según filtro

--- services/book-msvc/src/main/java/co/duvan/book/infrastructure/adapters/output/persistence/BookPersistenceAdapter.java
+++ services/book-msvc/src/main/java/co/duvan/book/infrastructure/adapters/output/persistence/BookPersistenceAdapter.java
@@ -2,13 +2,18 @@ package co.duvan.book.infrastructure.adapters.output.persistence;
 
 import co.duvan.book.application.ports.output.BookRepositoryPort;
 import co.duvan.book.domain.model.Book;
+import co.duvan.book.domain.model.BookFilterQuery;
+import co.duvan.book.infrastructure.adapters.output.persistence.entity.BookEntity;
 import co.duvan.book.infrastructure.adapters.output.persistence.mapper.BookPersistenceMapper;
 import co.duvan.book.infrastructure.adapters.output.persistence.repository.BookRepository;
+import co.duvan.book.infrastructure.adapters.output.persistence.repository.BookSpecification;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
@@ -35,6 +40,21 @@ public class BookPersistenceAdapter implements BookRepositoryPort {
     @Override
     public void deleteById(Long id) {
         repository.deleteById(id);
+    }
+
+    @Override
+    public List<Book> findWithFilters(BookFilterQuery filter) {
+
+        Specification<BookEntity> spec = Specification
+                .where(BookSpecification.hasTitle(filter.getTitle()))
+                .and(BookSpecification.hasCategory(filter.getCategory()))
+                .and(BookSpecification.hasAuthor(filter.getAuthor()));
+
+        return repository.findAll(spec)
+                .stream()
+                .map(bookPersistenceMapper::toBook)
+                .collect(Collectors.toList());
+
     }
 
 }
