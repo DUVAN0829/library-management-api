56100bf39a98939a7e6ccaa0f2637287c4e2025c
DUVAN0829 <duvthan067@gmail.com>
2026-03-01 07:26:25 -0500

    feat(user-msvc): Añade query method para obtener usuario por propiedad

--- services/user-msvc/src/main/java/co/duvan/user/infrastructure/adapters/output/persistence/repository/UserRepository.java
+++ services/user-msvc/src/main/java/co/duvan/user/infrastructure/adapters/output/persistence/repository/UserRepository.java
@@ -1,12 +1,30 @@
 package co.duvan.user.infrastructure.adapters.output.persistence.repository;
 
+import co.duvan.user.domain.enums.DocumentType;
 import co.duvan.user.infrastructure.adapters.output.persistence.entity.UserEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
 
     Optional<UserEntity> findByKeycloakId(String keycloakId);
+
+    @Query("""
+    SELECT u FROM UserEntity u
+    WHERE (:firstname IS NULL OR LOWER(u.firstname) LIKE LOWER(CONCAT('%', CAST(:firstname AS string), '%')))
+    AND (:lastname IS NULL OR LOWER(u.lastname) LIKE LOWER(CONCAT('%', CAST(:lastname AS string), '%')))
+    AND (:documentType IS NULL OR u.documentType = :documentType)
+    AND (:documentNumber IS NULL OR u.documentNumber = :documentNumber)
+""")
+    List<UserEntity> findWithFilters(
+            @Param("firstname") String firstname,
+            @Param("lastname") String lastname,
+            @Param("documentType") DocumentType documentType,
+            @Param("documentNumber") String documentNumber
+    );
 
 }
