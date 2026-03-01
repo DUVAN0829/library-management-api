61ce52135f1b16bcdec1afe866950858df85a774
DUVAN0829 <duvthan067@gmail.com>
2026-03-01 16:40:15 -0500

    feat(copy-msvc): Añade implementación para caso de uso de actualizar estado de ejemplar mediante evento ohtenido en kafka

--- services/copy-msvc/src/main/java/co/duvan/copy/application/usecase/UpdateCopyUseCaseImpl.java
+++ services/copy-msvc/src/main/java/co/duvan/copy/application/usecase/UpdateCopyUseCaseImpl.java
@@ -2,6 +2,7 @@ package co.duvan.copy.application.usecase;
 
 import co.duvan.copy.application.ports.input.UpdateCopyUseCase;
 import co.duvan.copy.application.ports.output.CopyRepositoryPort;
+import co.duvan.copy.domain.enums.Status;
 import co.duvan.copy.domain.exceptions.CopyNotFoundException;
 import co.duvan.copy.domain.model.Copy;
 import lombok.RequiredArgsConstructor;
@@ -15,13 +16,21 @@ public class UpdateCopyUseCaseImpl implements UpdateCopyUseCase {
 
     @Override
     public Copy update(Long id, Copy copy) {
-        return repositoryPort.findById(id)
-                .map(copyDb -> {
-                    copyDb.setBookId(copy.getBookId());
-                    copyDb.setCode(copy.getCode());
-                    copyDb.setStatus(copy.getStatus());
-                    return repositoryPort.save(copyDb);
-                }).orElseThrow(() -> new CopyNotFoundException("Copy not found with id: " + id));
+        return repositoryPort.findById(id).map(copyDb -> {
+            copyDb.setBookId(copy.getBookId());
+            copyDb.setCode(copy.getCode());
+            copyDb.setStatus(copy.getStatus());
+            return repositoryPort.save(copyDb);
+        }).orElseThrow(() -> new CopyNotFoundException("Copy not found with id: " + id));
+    }
+
+    @Override
+    public void updateStatus(Long copyId, String status) {
+
+        Copy copy = repositoryPort.findById(copyId).orElseThrow(() -> new RuntimeException("Copy not found"));
+        copy.setStatus(Status.valueOf(status));
+        repositoryPort.save(copy);
+
     }
 
 }
