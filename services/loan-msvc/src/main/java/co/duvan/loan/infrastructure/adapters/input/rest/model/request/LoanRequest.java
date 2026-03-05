bd15a82869cf00e7d0f3e9ff3bb15d4aff07e690
DUVAN0829 <duvthan067@gmail.com>
2026-03-04 19:32:12 -0500

    fix(loan-msvc): Corrije parámetros del objeto del request con swagger

--- services/loan-msvc/src/main/java/co/duvan/loan/infrastructure/adapters/input/rest/model/request/LoanRequest.java
+++ services/loan-msvc/src/main/java/co/duvan/loan/infrastructure/adapters/input/rest/model/request/LoanRequest.java
@@ -65,7 +65,6 @@ public class LoanRequest {
             example = "ACTIVE",
             requiredMode = Schema.RequiredMode.REQUIRED
     )
-    @NotNull(message = "Field loanStatus cannot be null")
     private Status loanStatus;
 
 }
