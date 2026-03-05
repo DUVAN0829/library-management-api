90f0afe1a3de4575ac699d2db8d2f51e3916758e
DUVAN0829 <duvthan067@gmail.com>
2026-03-04 19:41:29 -0500

    feat(loan-msvc): Añade configuración de seguridad para swagger

--- services/loan-msvc/src/main/java/co/duvan/loan/infrastructure/adapters/input/rest/config/OpenApiConfig.java
+++ services/loan-msvc/src/main/java/co/duvan/loan/infrastructure/adapters/input/rest/config/OpenApiConfig.java
@@ -1,11 +1,17 @@
 package co.duvan.loan.infrastructure.adapters.input.rest.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.OAuthFlow;
+import io.swagger.v3.oas.annotations.security.OAuthFlows;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition(
+        security = @SecurityRequirement(name = "oauth2"),
         info = @Info(
                 title = "Loan Management API",
                 version = "1.0.0",
@@ -19,6 +25,16 @@ import org.springframework.context.annotation.Configuration;
                         The application is built following Hexagonal Architecture (Ports & Adapters),
                         promoting a clear separation between domain, application, and infrastructure layers.
                         """
+        )
+)
+@SecurityScheme(
+        name = "oauth2",
+        type = SecuritySchemeType.OAUTH2,
+        flows = @OAuthFlows(
+                authorizationCode = @OAuthFlow(
+                        authorizationUrl = "http://localhost:8080/realms/library-management-api/protocol/openid-connect/auth",
+                        tokenUrl = "http://localhost:8080/realms/library-management-api/protocol/openid-connect/token"
+                )
         )
 )
 public class OpenApiConfig {
