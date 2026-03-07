fb415e0d32345d3c64897626c00cb97ae8fc29bf
DUVAN0829 <duvthan067@gmail.com>
2026-03-07 18:29:26 -0500

    refactor(copy-msvc): Modifica conexiones a feign

--- services/copy-msvc/src/main/java/co/duvan/copy/infrastructure/adapters/output/client/BookClientFeign.java
+++ services/copy-msvc/src/main/java/co/duvan/copy/infrastructure/adapters/output/client/BookClientFeign.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "book-msvc", url = "http://localhost:8081", configuration = FeignClientInterceptor.class)
+@FeignClient(name = "book-msvc", url = "${book-msvc.url}", configuration = FeignClientInterceptor.class)
 public interface BookClientFeign {
 
     @GetMapping("/books/api/v1/{id}")
