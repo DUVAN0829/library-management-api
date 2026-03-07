20129d0442113afa127f80e2875a48f68159e4a6
DUVAN0829 <duvthan067@gmail.com>
2026-03-07 18:25:09 -0500

    refactor(loan-msvc): Modifica conexión a feign mediante application.properties

--- services/loan-msvc/src/main/java/co/duvan/loan/infrastructure/adapters/output/client/CopyFeignClient.java
+++ services/loan-msvc/src/main/java/co/duvan/loan/infrastructure/adapters/output/client/CopyFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "copy-msvc", url = "http://localhost:8082", configuration = FeignClientInterceptor.class)
+@FeignClient(name = "copy-msvc", url = "${copy-msvc.url}", configuration = FeignClientInterceptor.class)
 public interface CopyFeignClient {
 
     @GetMapping("/copies/api/v1/{id}")
