5a1bb17217b8e7bfca5a1cfef70d3eb879baa6f1
DUVAN0829 <duvthan067@gmail.com>
2026-03-13 14:26:01 -0500

    refactor(observability-commons): Añade log con lombok

--- services/observability-commons/src/main/java/co/duvan/commons/monitoring/TraceIdFilter.java
+++ services/observability-commons/src/main/java/co/duvan/commons/monitoring/TraceIdFilter.java
@@ -3,6 +3,8 @@ package co.duvan.commons.monitoring;
 import io.opentelemetry.api.trace.Span;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -10,9 +12,8 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 @Component
+@Slf4j
 public class TraceIdFilter implements Filter {
-
-    private Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
